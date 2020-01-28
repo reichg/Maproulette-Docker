2278bf12005130b0812f995083230ccbcc920839
unknown <gaber@GABER-IGI.redmond.corp.microsoft.com>
2020-01-28 12:44:30 -0800

    working docker, need to fix project view, looks to be stuck in infinite loop

--- maproulette2/scripts/updateOSMKeys.sh
+++ maproulette2/scripts/updateOSMKeys.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Get the certificate for openstreetmap
-openssl s_client -showcerts -connect "www.openstreetmap.org:443" -servername www.openstreetmap.org </dev/null | sed -ne '/-BEGIN CERTIFICATE-/,/-END CERTIFICATE-/p' > ../conf/osm.pem
-keytool -delete -alias www.openstreetmap.org -keystore ../conf/osmcacerts -storepass openstreetmap
-keytool -importcert -noprompt -trustcacerts -alias www.openstreetmap.org -file ../conf/osm.pem -keystore ../conf/osmcacerts -storepass openstreetmap
+# openssl s_client -showcerts -connect "www.openstreetmap.org:443" -servername www.openstreetmap.org </dev/null | sed -ne '/-BEGIN CERTIFICATE-/,/-END CERTIFICATE-/p' > ../conf/osm.pem
+# keytool -delete -alias www.openstreetmap.org -keystore ../conf/osmcacerts -storepass openstreetmap
+# keytool -importcert -noprompt -trustcacerts -alias www.openstreetmap.org -file ../conf/osm.pem -keystore ../conf/osmcacerts -storepass openstreetmap
 
 # Get the certificate for dev openstreetmap servers
 openssl s_client -showcerts -connect "master.apis.dev.openstreetmap.org:443" -servername master.apis.dev.openstreetmap.org </dev/null | sed -ne '/-BEGIN CERTIFICATE-/,/-END CERTIFICATE-/p' > ../conf/osm_dev.pem
